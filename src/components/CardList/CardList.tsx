3315eaf9b0dc8d93a2f24bbadbcd70afb7cf7c29
tavaresflavia <altavares.flavia@gmail.com>
2024-02-06 21:14:25 -0500

    added get request to card list

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -9,70 +9,29 @@ const CardList = () => {
   const [products, setProducts] = useState<(Product)[]>([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [type, setType] = useState("");
   const [brand, setBrand] = useState("");
   const [category, setCategory] = useState("");
-  const [min, setMin] = useState(0);
-  const [maxi, setMaxi] = useState(999);
-  const [rating, setRating] = useState(0);
   const [tags, setTags] = useState([]);
 
   useEffect(() => {
+
+      const queryTags = tags?.map((tag)=> (`&tags=${tag}`))
+      // .sort((a:Product,b:Product) => (b.rating-a.rating))
     axios
       .get(
-        `https://makeup-api.herokuapp.com/api/v1/products.json?price_less_than=${maxi}&price_gretaer_than=${min}${
-          type ? `&product_type=${type}` : ""
-        }${category ? `&product_category=${category}` : ""}${
-          brand ? `&brand=${brand}` : ""
-        }${rating ? `&rating_greater_than=${rating}` : ""}`
+        `http://localhost:8080/products?${category ? "category="+category:""}${brand ? "brand="+brand:""}${String(...queryTags)}`
       )
       .then((res) => {
-        const data = res.data.map((product: Product) => {
-          const {
-            id,
-            brand,
-            name,
-            price,
-            description,
-            category,
-            type,
-            tag_list: tags,
-            api_featured_image: imageUrl,
-            product_colors: colors,
-            rating,
-          } = product;
-          return {
-            id,
-            brand,
-            name,
-            price,
-            description,
-            category,
-            type,
-            tags,
-            imageUrl,
-            colors,
-            rating,
-          };
-          });
-
-        if (tags.length > 0) {
-          const filteredData = data.filter(
-            (product: { tag_list: string[] }) => {
-              return tags.every((tag) => product.tag_list.includes(tag));
-            }
-          );
-          setProducts(filteredData);
-        } else {
-          setProducts(data);
-        }
+        console.log(res)
+        setProducts(res.data);
         setLoading(false);
       })
-      .catch(() => {
+      .catch((error:string) => {
+        console.log(error)
         setError(true);
         setLoading(false);
       });
-  }, [min, maxi, type, tags, rating]);
+  }, [ tags, category, brand]);
 
   if (loading === true){
     return (
